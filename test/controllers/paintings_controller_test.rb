ce8e299c0def860ec9badecb73a6c284854705a0
Tobias Bühlmann <77664+tbuehlmann@users.noreply.github.com>
2024-10-01 16:11:54 +0200

    Fix tests

--- test/controllers/paintings_controller_test.rb
+++ test/controllers/paintings_controller_test.rb
@@ -10,39 +10,11 @@ class PaintingsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_painting_url
-    assert_response :success
-  end
-
   test "should create painting" do
     assert_difference("Painting.count") do
-      post paintings_url, params: { painting: { color: @painting.color } }
+      post paintings_url
     end
 
-    assert_redirected_to painting_url(Painting.last)
-  end
-
-  test "should show painting" do
-    get painting_url(@painting)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_painting_url(@painting)
-    assert_response :success
-  end
-
-  test "should update painting" do
-    patch painting_url(@painting), params: { painting: { color: @painting.color } }
-    assert_redirected_to painting_url(@painting)
-  end
-
-  test "should destroy painting" do
-    assert_difference("Painting.count", -1) do
-      delete painting_url(@painting)
-    end
-
-    assert_redirected_to paintings_url
+    assert_redirected_to root_url
   end
 end
--- test/system/paintings_test.rb
+++ test/system/paintings_test.rb
@@ -9,33 +9,4 @@ class PaintingsTest < ApplicationSystemTestCase
     visit paintings_url
     assert_selector "h1", text: "Paintings"
   end
-
-  test "should create painting" do
-    visit paintings_url
-    click_on "New painting"
-
-    fill_in "Color", with: @painting.color
-    click_on "Create Painting"
-
-    assert_text "Painting was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Painting" do
-    visit painting_url(@painting)
-    click_on "Edit this painting", match: :first
-
-    fill_in "Color", with: @painting.color
-    click_on "Update Painting"
-
-    assert_text "Painting was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Painting" do
-    visit painting_url(@painting)
-    click_on "Destroy this painting", match: :first
-
-    assert_text "Painting was successfully destroyed"
-  end
 end
